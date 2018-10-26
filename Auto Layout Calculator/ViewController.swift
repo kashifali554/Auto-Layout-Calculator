fa6dbf2655000145fbc5f7d7eb5dc8b7ada762e9
Kashif Ali <kashifali874@gmail.com>
2018-10-25 22:33:24 -0700

    first comment

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Ali on 10/25/18.
+//  Copyright © 2018 Kashif Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
